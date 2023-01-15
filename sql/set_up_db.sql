3ddd76646c38fe022bcade3ffcb97f51c5ef719f
gallit0 <mateogesede@gmail.com>
2023-01-15 19:41:03 -0300

    sql script updated

--- sql/set_up_db.sql
+++ sql/set_up_db.sql
@@ -1,5 +1,34 @@
+CREATE DATABASE IF NOT EXISTS `social`;
+USE `social`;
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `name` varchar(45) NOT NULL,
+  `coverPic` varchar(100) DEFAULT NULL,
+  `profilePic` varchar(100) DEFAULT NULL,
+  `city` varchar(45) DEFAULT NULL,
+  `website` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `posts` (
+  `id` int NOT NULL,
+  `desc` varchar(200) DEFAULT NULL,
+  `img` varchar(200) DEFAULT NULL,
+  `userid` int NOT NULL,
+  `createdAt` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `userid_idx` (`userid`),
+  CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 CREATE TABLE `comments` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `desc` varchar(200) NOT NULL,
   `createdAt` datetime DEFAULT NULL,
   `userId` int NOT NULL,
@@ -12,10 +41,11 @@ CREATE TABLE `comments` (
   CONSTRAINT `postId` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `likes` (
-  `id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `decisions` (
+  `id` int NOT NULL,
   `userId` int NOT NULL,
   `postId` int NOT NULL,
+  `yes_no` boolean not null,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `likeeUserId_idx` (`userId`),
@@ -24,20 +54,8 @@ CREATE TABLE `likes` (
   CONSTRAINT `likePostId` FOREIGN KEY (`postId`) REFERENCES `posts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `posts` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `desc` varchar(200) DEFAULT NULL,
-  `img` varchar(200) DEFAULT NULL,
-  `userid` int NOT NULL,
-  `createdAt` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  KEY `userid_idx` (`userid`),
-  CONSTRAINT `userid` FOREIGN KEY (`userid`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 CREATE TABLE `relationships` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `followerUserId` int NOT NULL,
   `followedUserId` int NOT NULL,
   PRIMARY KEY (`id`),
@@ -49,7 +67,7 @@ CREATE TABLE `relationships` (
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `stories` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL,
   `img` varchar(200) NOT NULL,
   `userId` int NOT NULL,
   PRIMARY KEY (`id`),
@@ -58,16 +76,3 @@ CREATE TABLE `stories` (
   CONSTRAINT `storyUserId` FOREIGN KEY (`userId`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(45) NOT NULL,
-  `email` varchar(45) NOT NULL,
-  `password` varchar(200) NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `coverPic` varchar(100) DEFAULT NULL,
-  `profilePic` varchar(100) DEFAULT NULL,
-  `city` varchar(45) DEFAULT NULL,
-  `website` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
