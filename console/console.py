8cab29b5d586f7be00a0e5dbd2f1212469aaa94a
gallit0 <mateogesede@gmail.com>
2023-01-17 23:39:51 -0300

    db has to be changed and improved in order to make the console work properly

--- console/console.py
+++ console/console.py
@@ -4,50 +4,72 @@ import cmd
 import os
 import uuid
 
+try:
+  if os.geteuid() != 0:
+    raise Exception('not root user')
+  db = MySQLdb.connect(host="localhost",
+                      port=3306,
+                      user='root',
+                      passwd='root',
+                      db='social')
+  cur = db.cursor()
+except Exception as e:
+  print(f'*** database error:\n{e}')
+  exit(2)
+cur.execute("SELECT table_name \
+    FROM information_schema.tables \
+    WHERE table_type='BASE TABLE' \
+    AND table_schema = 'social'")
+
+
 class CommandLine(cmd.Cmd):
   prompt = '# '
-  try:
-    db = MySQLdb.connect(host="localhost",
-                        port=3306,
-                        user='root',
-                        passwd='root',
-                        db='social')
-    cur = db.cursor()
-  except Exception as e:
-    print(f'*** database error:\n{e}')
-    exit(2) 
-  cur.execute("SELECT table_name \
-      FROM information_schema.tables \
-      WHERE table_type='BASE TABLE' \
-      AND table_schema = 'social'")
-  tables = list(map(lambda x: x[0], cur.fetchall()))
+  tables = set(map(lambda x: x[0], cur.fetchall()))
 
   def do_EOF(self, line):
+    """*** ctrl + d to exit the console"""
+    cur.close()
     db.close()
     return True
   
   def do_quit(self, args):
-    """Quit command to exit the program"""
+    """*** exit the console"""
+    cur.close()
     db.close()
     return True
 
   def do_clear(self, args):
+    """***clear screen"""
     os.system('clear')
 
   def do_tables(self, args):
+    """*** display set with the name of all the tables in the database"""
     print(CommandLine.tables)
 
-  def do_create(self, args):
-    line = args.split(' ')
-    if len(line[0]) < 1:
-      print('** no table name **')
+  #def do_create(self, args):
+  #  """*** create a new row in the database
+  #  Usage: create <table_name> <attributes>
+  #  Ex: create users nick=John"""
+  #  line = args.split(' ')
+  #  if len(line[0]) < 1:
+  #    print('** no table name **')
+  #    return
+  #  if args not in CommandLine.tables:
+  #    print('** table name not valid **')
+  #    return
+  #  cur.execute(f'INSERT INTO {line[]}')
+    
+  def do_delete(self, args):
+    """*** delete row in database
+    Usage: delete <table_name> <id>"""
+    pass
+
 
 
 
 
 if __name__ == '__main__':
   if os.geteuid() == 0:
-    
     CommandLine().cmdloop()
   else:
     print('** not root user **')
