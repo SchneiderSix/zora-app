3ba2607d2e4312047b89b7536b9f14708502ffbc
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2023-01-31 17:37:11 -0300

    Create random Id for comments

--- api/controllers/comment.js
+++ api/controllers/comment.js
@@ -2,6 +2,12 @@ import { db } from "../connect.js";
 import jwt from "jsonwebtoken";
 import moment from "moment";
 
+function getRandomId(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 export const getComments = (req, res) => {
   const q = `SELECT c.*, u.id AS userId, name, profilePic FROM comments AS c JOIN users AS u ON (u.id = c.userId)
     WHERE c.postId = ? ORDER BY c.createdAt DESC
@@ -19,13 +25,30 @@ export const addComment = (req, res) => {
 
   jwt.verify(token, "secretkey", (err, userInfo) => {
     if (err) return res.status(403).json("Token is not valid!");
+    let redo = 0;
+    do {
+      // min 10000000
+      // max 99999999
+      var uid = getRandomId(10000000, 99999999);
+      let q_id = "SELECT * FROM `posts` WHERE `id`=?";
+      db.query(q_id, [+uid], (err, data) => {
+        if (err) return res.status(500).json(err);
+        if (data) {
+          redo = 1;
+        } else {
+          redo = 0;
+        }
+      });
+    } while (redo == 1);
 
-    const q = "INSERT INTO comments(`desc`, `createdAt`, `userId`, `postId`) VALUES (?)";
+    const q =
+      "INSERT INTO comments(`id`, `desc`, `createdAt`, `userId`, `postId`) VALUES (?)";
     const values = [
+      uid,
       req.body.desc,
       moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
       userInfo.id,
-      req.body.postId
+      req.body.postId,
     ];
 
     db.query(q, [values], (err, data) => {
